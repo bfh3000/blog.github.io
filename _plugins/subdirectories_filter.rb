7e31582714ea5987247896b1bffdcd0dd64a2b8e
dhlee <bfh3000@naver.com>
2023-08-25 14:36:02 +0900

    update category 동적으로 가져오는 리스트.

--- _plugins/subdirectories_filter.rb
+++ _plugins/subdirectories_filter.rb
@@ -3,6 +3,9 @@ module Jekyll
     def subdirectories(input)
       Dir.glob(File.join(input, '*')).select { |f| File.directory?(f) }
     end
+    def file_list(input)
+      Dir.glob(File.join(input, '*')).select { |f| File.file?(f) }.map { |f| File.basename(f) }
+    end
   end
 end
 
